473c86e90fe14eaf8ef64a97134d79b8c98f18d6
Mehul Jain <mehul11jain@outlook.com>
2021-04-21 05:17:11 +0530

    Printing To Dot file of Points-To Graph in Flow Insensitive Analysis

--- PTlib/src/FIS_PTReader.cpp
+++ PTlib/src/FIS_PTReader.cpp
@@ -124,10 +124,145 @@ void PTDump::FIS_PTReader::printPointsToDump()
 
 void PTDump::FIS_PTReader::printToDot()
 {
+    std::map<std::string, int> ValID;
+    std::string dash = "-";
+    int i = 1;
+    for(auto pt : reader["FlowInsensitivePointsToInfo"]["PointsToGraph"])
+    {
+        std::string pt_string;
+        pt_string = pt["name"];
+        pt_string += "-";
+        pt_string += pt["procedure"];
 
+        if(pt["procedure"] == "" && ValID.find(pt["name"]) == ValID.end())
+            ValID[pt["name"]] = i++;
+        else if(pt["procedure"] != "" && ValID.find(pt_string) == ValID.end())
+            ValID[pt_string] = i++;
+
+        for(auto it : pt["PointsToSet"])
+        {
+            std::string it_string;
+            it_string = it["name"];
+            it_string += "-";
+            it_string += it["procedure"];
+            if(it["procedure"] == "" && ValID.find(it["name"]) == ValID.end())
+                ValID[it["name"]] = i++;
+            else if(it["procedure"] != "" && ValID.find(it_string) == ValID.end())
+                ValID[it_string] = i++;
+        }
+    }
+    std::ofstream fstr;
+    fstr.open("test.dot");
+    fstr << "digraph {\n";    
+    fstr << "\trankdir=\"LR\";\n";
+    fstr << "\tnode[shape=circle, width=0.5, color=lightblue2, style=filled];\n";
+    for(auto it : ValID)
+    {
+        fstr << it.second << "[label=\"" << it.first << "\"];\n";
+    }
+    for(auto pt : reader["FlowInsensitivePointsToInfo"]["PointsToGraph"])
+    {
+        std::string pt_name;        
+        pt_name = pt["name"];
+        if(pt["procedure"] != "")
+        {
+            pt_name += "-"; 
+            pt_name += pt["procedure"];
+        }
+
+        for(auto it : pt["PointsToSet"])
+        {
+            std::string pts_string;            
+            pts_string = it["name"];
+            if(it["procedure"] != "")
+            {
+                pts_string += "-";
+                pts_string += it["procedure"];
+            }
+
+            fstr << ValID[pt_name] << "->" << ValID[pts_string] << ";\n";
+        }
+    }
+    fstr << "}\n";
+    fstr.close();
 }
 
 bool PTDump::FIS_PTReader::isPointee(const llvm::Value* Pointer, const llvm::Value* Pointee)
 {
-    return 1;
+    bool is_global_pointer = llvm::isa<llvm::GlobalVariable>(Pointer);
+    bool is_global_pointee = llvm::isa<llvm::GlobalVariable>(Pointee);
+    if(is_global_pointer && is_global_pointee)
+    {
+        auto PTGraph = reader["FlowInsensitivePointsToInfo"]["PointsToGraph"];
+        for(auto pt : PTGraph)
+        {
+            if(pt["name"] == Pointer->getName())
+            {
+                for(auto i : pt["PointsToSet"])
+                {
+                    if(i["name"] == Pointee->getName())
+                    {
+                        return 1;
+                    }
+                }
+            }
+        }
+
+    }
+    else if(!is_global_pointer && is_global_pointee)
+    {
+        const llvm::Function* fun = llvm::dyn_cast<llvm::Instruction>(Pointer)->getFunction();
+        auto PTGraph = reader["FlowInsensitivePointsToInfo"]["PointsToGraph"];
+        for(auto pt : PTGraph)
+        {
+            if(pt["name"] == Pointer->getName() && fun->getName().str() == pt["procedure"])
+            {
+                for(auto i : pt["PointsToSet"])
+                {
+                    if(i["name"] == Pointee->getName())
+                    {
+                        return 1;
+                    }
+                }
+            }
+        }
+    }
+    else if(is_global_pointer && !is_global_pointee)
+    {
+        const llvm::Function* fun = llvm::dyn_cast<llvm::Instruction>(Pointee)->getFunction();
+        auto PTGraph = reader["FlowInsensitivePointsToInfo"]["PointsToGraph"];
+        for(auto pt : PTGraph)
+        {
+            if(pt["name"] == Pointer->getName())
+            {
+                for(auto i : pt["PointsToSet"])
+                {
+                    if(i["name"] == Pointee->getName() && fun->getName().str() == i["procedure"])
+                    {
+                        return 1;
+                    }
+                }
+            }
+        }
+    }
+    else
+    {
+        const llvm::Function* fun  = llvm::dyn_cast<llvm::Instruction>(Pointee)->getFunction();
+        const llvm::Function* fun2 = llvm::dyn_cast<llvm::Instruction>(Pointer)->getFunction();
+        auto PTGraph = reader["FlowInsensitivePointsToInfo"]["PointsToGraph"];
+        for(auto pt : PTGraph)
+        {
+            if(pt["name"] == Pointer->getName() && fun2->getName().str() == pt["procedure"])
+            {
+                for(auto i : pt["PointsToSet"])
+                {
+                    if(i["name"] == Pointee->getName() && fun->getName().str() == i["procedure"])
+                    {
+                        return 1;
+                    }
+                }
+            }
+        }
+    }
+    return 0;
 }
